47b9d3a076ce25449cff7bf11cdbb32e27c01127
Miquel Brazil <mbrazil@thingwone.com>
2014-07-01 10:52:15 -0400

    initial commit
    version control check-in

---
+++ champ-lite4hive.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package CHAMP LITE4HIV
+ * @version 0.0.2
+ */
+/*
+Plugin Name: CHAMP LITE4HIV: Client Health Activity Management Platform - Lightweight Information Transcription Environment for HIV Testing
+Plugin URI: http://healthspotohio.org/technology
+Description: Plugin for accessing ATGC-specific, HIV testing information from Formstack and converting it to ODH/CDC compatible EvaluationWeb documentation
+Author: Miquel Brazil
+Version: 0.0.2
+Author URI: http://wone.co
+*/
